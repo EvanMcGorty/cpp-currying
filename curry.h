6b9703e2d32facb171ea482ae16e8bd253bc93e7
Evan McGorty <evanjamesmcgorty@gmail.com>
2022-09-09 11:05:16 -0400

    make curry flatten curry_wrappers, add generic curried<> overload

--- curry.h
+++ curry.h
@@ -1,3 +1,4 @@
+#pragma once
 #include<concepts>
 #include<functional>
 
@@ -86,21 +87,25 @@ public:
 
 //Should just be a concept, however it needs to manually handle its variadic arguments
 //Rather an implementation detail and can be ignored by the user
-template<typename t, typename return_t, typename...args_t>
+template<typename t, typename...ts>
 constexpr bool is_curried_v = false;
 
 //Predeclare 'curried' in order to use concept syntax in the other definitions of is_curried_v
 //Strictly speaking not necessary
 
-//A concept for curried functions that take particular parameters
+//A concept for functions created by 'curry' and return types of their applications
+//No parameters just means that it is any curried function, regardless of parameter types
+//Otherwise the first parameter is the return type, and the rest are the argument types
 //void can be used to indicate a unit parameter, aka an empty () or invocation with zero arguments
-//This is not a general purpose curry concept, and will only work with functions curried using 'curry'
 //A curried<A,B,C,D> corresponds to a "B -> C -> D -> A"
 //A curried<A> just corresponds to an "A", though still wrapped up by curry, so an implicit conversion is needed
 //A std::function<std::function<A(B)>(C)> is curried<A,C,B> as well as curried<std::function<A(B)>,C>
 //A std::function<int()> is curried<int,void>
-template<typename t, typename return_t, typename...args_t>
-concept curried = is_curried_v<t, return_t, args_t...>;
+template<typename t, typename...ts>
+concept curried = is_curried_v<t, ts...>;
+
+template<typename t>
+constexpr bool is_curried_v<curry_wrapper<t>> = true;
 
 template<typename t, typename return_t>
 constexpr bool is_curried_v<curry_wrapper<t>, return_t> = std::same_as<t,return_t>;  //and implicitly, std::convertible_to<curry_wrapper<t>,return_t>
@@ -118,9 +123,17 @@ constexpr bool is_curried_v<curry_wrapper<t>, return_t, arg1_t, args_t...> = req
 };
 
 //Curries a function so that it may be called with any number of parameters
-//Functions with no parameters are treated as if they take a unit type, and must be manually empty-invoked
+//Functions with no parameters are treated as if they take a unit type, and must be explicitly empty-invoked
+//Already curried functions will simply be returned
 template<typename callable_t>
 constexpr auto curry(callable_t&& callable)
 {
-	return curry_wrapper<callable_t>(std::forward<callable_t>(callable));
+	if constexpr(curried<callable_t>)
+	{
+		return std::forward<callable_t>(callable);
+	}
+	else
+	{
+		return curry_wrapper<callable_t>(std::forward<callable_t>(callable));
+	}
 }
