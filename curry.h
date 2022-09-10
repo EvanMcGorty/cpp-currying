4995eddb70097150dfe82d6ea89194a889a9b40b
Evan McGorty <evanjamesmcgorty@gmail.com>
2022-09-10 16:37:52 -0400

    reorganize, add comments

--- curry.h
+++ curry.h
@@ -1,9 +1,61 @@
+/*
+This is a single-header, powerful and lightweight library that provides support for currying.
+It can be found at https://github.com/EvanMcGorty/cpp-currying
+It is designed to be simply plopped into a project and included.
+
+Overview of this file:
+
+curry: returns a curried version of a function, in the form of a curried_wrapper
+
+curried: a concept for a curried version of a function, equivalent to the constexpr bool is_curried_v
+
+curry_wrapper<t>::operator(): partially apply any number of arguments to and possibly evaluate a curried function
+
+curry_wrapper<t>::curry_wrapped_val(): access the value wrapped up by curry
+
+curry_wrapper<t>::curry_wrapped_type: the type returned by curry_wrapped_val()
+
+curry_wrapper<t>::curry_wrapped_type_including_references: the type of the value actually stored in the wrapper, same as the template parameter
+
+curry_wrapper<t> default/copy/move constructors, copy/move assignment operators, and a destructor
+*/
+
 #pragma once
 #include<concepts>
 #include<functional>
 
+
+//Should just be a concept, however it needs to manually handle its variadic arguments
+//Rather an implementation detail and can be ignored by the user
+//Checks that each argument and the return type are correct when applied one at a time and that target type is an instance of curry_wrapper
+//Ideally it would just check the semantics of the class but it is too slow to check every possible way to apply the parameters.
+//For example: c(t1,t2,t3,t4), c(t1)(t2,t3,t4), c(t1,t2)(t3)(t4), etc, this grows exponentially with more parameters
+template<typename t, typename...ts>
+constexpr bool is_curried_v = false //Base case overload, where t doesnt match the form of curry_wraper<something>
+	//There is an exception: t might have qualifiers that need to be removed (ie due to being perfect forwarded)
+	|| !std::same_as<t,std::remove_cvref_t<t>> && is_curried_v<std::remove_cvref_t<t>,ts...>;
+
+//A concept for functions created by 'curry' and return types of their applications
+//No parameters just means that it is any curried function, regardless of parameter types
+//Otherwise the first parameter is the return type, and the rest are the argument types
+//void can be used to indicate a unit parameter, aka an empty () or invocation with zero arguments
+//A curried<A,B,C,D> corresponds to a "B -> C -> D -> A"
+//A curried<A> just corresponds to an "A", though still wrapped up by curry, so an implicit conversion is needed
+//A std::function<std::function<A(B)>(C)> is curried<A,C,B> as well as curried<std::function<A(B)>,C>
+//A std::function<int()> is curried<int,void>
+template<typename t, typename...ts>
+concept curried = is_curried_v<t, ts...>;
+
+//Curries a function so that it may be called with any number of parameters
+//Therefore, it (ironically) also technically functions as an uncurry
+//Functions with no parameters are treated as if they take a unit type, and must be explicitly empty-invoked
+//If the target callable is passed by (lvalue) reference, then it will be stored by reference
+//To have it stored by value, std::move or copy construct if it isnt already being passed as an rvalue
+//Beware: alling curry on an already curried function will simply return it via perfect forwarding
+//Applications are done via bind_front, so std::ref and std::cref can be used to avoid copies.
+//Consider std::move-ing your curried callables when calling operator() to avoid copying previous captures into the new callable
 template<typename callable_t>
-constexpr auto curry(callable_t&& callable);
+constexpr curried auto curry(callable_t&& callable);
 
 //A class to wrap a (possibly) callable object
 //Has multiple overloads of operator() for different numbers of parameters
@@ -20,7 +72,7 @@ private:
 	
 	//Defer all construction to this function
 	template<typename friend_callable_t>
-	friend constexpr auto curry(friend_callable_t&& callable);
+	friend constexpr curried auto curry(friend_callable_t&& callable);
 
 	template<typename friend_callable_t>
 	friend class curry_wrapper;
@@ -66,27 +118,29 @@ private:
 		}
 	}
 
+	//Made private and generally not used in order to propogate qualifiers from member calls
 	callable_t raw_wrapped_val;
 
 public:
 
 	//The type of the wrapped callable value
-	using curry_wrapped_type = callable_t;
+	using curry_wrapped_type = std::remove_reference_t<callable_t>;
+	using curry_wrapped_type_including_references = callable_t;
 	
-	std::remove_reference_t<curry_wrapped_type>& curry_wrapped_val() &
+	curry_wrapped_type& curry_wrapped_val() &
 	{
 		return raw_wrapped_val;
 	}
-	std::remove_reference_t<curry_wrapped_type> const& curry_wrapped_val() const&
+	curry_wrapped_type const& curry_wrapped_val() const&
 	{
 		auto const& ret = raw_wrapped_val;
 		return ret;
 	}
-	std::remove_reference_t<curry_wrapped_type>&& curry_wrapped_val() &&
+	curry_wrapped_type&& curry_wrapped_val() &&
 	{
 		return std::move(raw_wrapped_val);
 	}
-	std::remove_reference_t<curry_wrapped_type> const&& curry_wrapped_val() const&&
+	curry_wrapped_type const&& curry_wrapped_val() const&&
 	{
 		auto const& ret = raw_wrapped_val;
 		return std::move(ret);
@@ -95,28 +149,22 @@ public:
 	//Implicit conversions to the wrapped value
 	//Access the result of using a function through 'curry'
 	
-	constexpr operator std::remove_reference_t<curry_wrapped_type>&() &
+	constexpr operator curry_wrapped_type&() &
 	{
 		return curry_wrapped_val();
 	}
-	constexpr operator std::remove_reference_t<curry_wrapped_type> const&() const&
+	constexpr operator curry_wrapped_type const&() const&
 	{
 		return curry_wrapped_val();
 	}
-	constexpr operator std::remove_reference_t<curry_wrapped_type>&&() &&
+	constexpr operator curry_wrapped_type&&() &&
 	{
 		return std::move(curry_wrapped_val());
 	}
-	constexpr operator std::remove_reference_t<curry_wrapped_type> const&&() const&&
+	constexpr operator curry_wrapped_type const&&() const&&
 	{
 		return std::move(curry_wrapped_val());
 	}
-
-	//todo: as_ref, as_const_ref, as_rvalue_ref, as_val
-	//add constructors and assignment operators from other instances of curry_wrapper
-	//	keep default implementation but in templated version require that other is different than this
-	//	this way we keep qualities like (for instance) being trivially move constructible when the held value allows it
-
 
 
 	template<typename...args_t>
@@ -151,29 +199,6 @@ public:
 	constexpr ~curry_wrapper() = default;
 };
 
-//Should just be a concept, however it needs to manually handle its variadic arguments
-//Rather an implementation detail and can be ignored by the user
-//Checks that each argument and the return type are correct when applied one at a time and that target type is an instance of curry_wrapper
-//Ideally it would just check the semantics of the class but it is too slow to check every possible way to apply the parameters.
-//For example: c(t1,t2,t3,t4), c(t1)(t2,t3,t4), c(t1,t2)(t3)(t4), etc, this grows exponentially with more parameters
-template<typename t, typename...ts>
-constexpr bool is_curried_v = false //Base case, where t doesnt match the form of curry_wraper<something>
-	//There is an exception: t might have qualifiers that need to be removed (ie due to being perfect forwarded)
-	|| !std::same_as<t,std::remove_cvref_t<t>> && is_curried_v<std::remove_cvref_t<t>,ts...>;
-
-//Predeclare 'curried' in order to use concept syntax in the other definitions of is_curried_v (strictly speaking not necessary)
-
-//A concept for functions created by 'curry' and return types of their applications
-//No parameters just means that it is any curried function, regardless of parameter types
-//Otherwise the first parameter is the return type, and the rest are the argument types
-//void can be used to indicate a unit parameter, aka an empty () or invocation with zero arguments
-//A curried<A,B,C,D> corresponds to a "B -> C -> D -> A"
-//A curried<A> just corresponds to an "A", though still wrapped up by curry, so an implicit conversion is needed
-//A std::function<std::function<A(B)>(C)> is curried<A,C,B> as well as curried<std::function<A(B)>,C>
-//A std::function<int()> is curried<int,void>
-template<typename t, typename...ts>
-concept curried = is_curried_v<t, ts...>;
-
 template<typename t>
 constexpr bool is_curried_v<curry_wrapper<t>> = true;
 
@@ -192,16 +217,8 @@ constexpr bool is_curried_v<curry_wrapper<t>, return_t, arg1_t, args_t...> = req
 	{a(std::declval<arg1_t>())} -> curried<return_t, args_t...>;
 };
 
-//Curries a function so that it may be called with any number of parameters
-//Therefore, it also technically functions as an uncurry
-//Functions with no parameters are treated as if they take a unit type, and must be explicitly empty-invoked
-//If the target callable is passed by (lvalue) reference, then it will be stored by reference
-//To have it stored by value, std::move or copy construct if it isnt already being passed as an rvalue
-//Beware: alling curry on an already curried function will simply return it via perfect forwarding
-//Applications are done via bind_front, so std::ref and std::cref can be used to avoid copies.
-//Consider std::move-ing your curried callables when calling operator() to avoid copying previous captures into the new callable
 template<typename callable_t>
-constexpr auto curry(callable_t&& callable)
+constexpr curried auto curry(callable_t&& callable)
 {
 	if constexpr(is_curried_v<callable_t>)
 	{
